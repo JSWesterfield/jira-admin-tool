c8fc7ae9f77dfc83bd530314edac60046603a95e
Jake Westerfield <Jacob_Westerfield@discovery.com>
2022-03-03 23:30:49 -0500

    adds in config library to pull from config json files set JiraClient to set values to individual vars that the config library dumps values into

--- index.js
+++ index.js
@@ -1,21 +1,34 @@
 const express = require("express");
+const config = require("config");
 const app = express();
 const cors = require("cors");
 const { default: JiraClient } = require("jira-connector");
 
 app.use(cors());
 
+const host = config.get("JiraCloudCreds.host");
+const userName = config.get("JiraCloudCreds.username");
+const password = config.get("JiraCloudCreds.password");
+
+console.log(`1. My host name is ${host}` + `, and my username is ${userName}` + `, and I have a password of ${password}.`);
+
 var jira = new JiraClient({
-    host: "companyName.atlassian.net",                    // find host url
+    host: host,                    // find host url
     basic_auth: {
-        username: "Full_Name@company.com",     // find username(email?)
-        password: "Fake-Passowrd010101",                 // OAuth Token(from Custom or Connected JIRA App)
+        username: userName,     // find username(email?)
+        password: password,                 // OAuth Token(from Custom or Connected JIRA App)
     },
     strictSSL: false,
 });
 
+console.log(`2. My host name is ${host}` + `, and my username is ${userName}` + `, and I have a password of ${password}.`);
+
 app.get("/", (req, res) => {
     res.send("starts new nodejs project");
+
+    jira.issue.createIssue({
+        fields
+    })
 });
 
 app.listen(5000, () => console.log("listening on part 5000"));
